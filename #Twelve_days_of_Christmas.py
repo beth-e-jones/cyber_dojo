ea73dcbea6524afc07a6f78c308c1cd7fd8d2f3a
Beth Jones <beth.e.jones@ons.gov.uk>
2023-06-15 13:14:48 +0100

    adding initial solution

---
+++ #Twelve_days_of_Christmas.py
@@ -0,0 +1,69 @@
+#Twelve days of Christmas
+
+#Creating dictionary of values for each gift
+christmas_gifts = {"partridge": "a partridge in a pear tree",
+                   "turtle": "two turtle doves, and",
+                   "hens": "three french hens,",
+                   "birds": "four calling birds,",
+                   "rings": "FIVE GOLDEN RINGS,",
+                   "geese": "six geese a-laying,", 
+                   "swans": "seven swans a-swimming,",
+                   "maids": "eight maids a-milking,",
+                   "ladies": "nine ladies dancing,",
+                   "lords": "ten lords a-leaping,",
+                   "pipers": "eleven pipers piping,",
+                   "drummers": "twelve drummers drumming"}
+
+
+print ("On the first day of Christmas my true love gave to me")
+print (christmas_gifts["partridge"])
+
+print ("\n"," On the second day of Christmas my true love gave to me")
+print (christmas_gifts["turtle"],"\n",christmas_gifts["partridge"])
+
+print ("On the third day of Christmas my true love gave to me")
+print (christmas_gifts["hens"],"\n", christmas_gifts["turtle"],"\n",christmas_gifts["partridge"])
+
+print ("On the fourth day of Christmas my true love gave to me")
+print (christmas_gifts["birds"], "\n",christmas_gifts["hens"],"\n",christmas_gifts["turtle"],
+       "\n",christmas_gifts["partridge"])
+
+print ("\n","On the fifth day of Christmas my true love gave to me")
+print (christmas_gifts["rings"],christmas_gifts["birds"], christmas_gifts["hens"],
+       christmas_gifts["turtle"],christmas_gifts["partridge"])
+
+print ("\n", "On the sixth day of Christmas my true love gave to me")
+print (christmas_gifts["geese"], christmas_gifts["rings"],christmas_gifts["birds"], 
+       christmas_gifts["hens"],christmas_gifts["turtle"],christmas_gifts["partridge"])
+
+print ("On the seventh day of Christmas my true love gave to me")
+print (christmas_gifts["swans"], christmas_gifts["geese"], christmas_gifts["rings"],
+       christmas_gifts["birds"], christmas_gifts["hens"],christmas_gifts["turtle"],
+       christmas_gifts["partridge"])
+
+print ("On the eighth day of Christmas my true love gave to me")
+print (christmas_gifts["swans"], christmas_gifts["geese"], christmas_gifts["rings"],
+       christmas_gifts["birds"], christmas_gifts["hens"],christmas_gifts["turtle"],
+       christmas_gifts["partridge"])
+
+print ("On the ninth day of Christmas my true love gave to me")
+print (christmas_gifts["ladies"], christmas_gifts["swans"], christmas_gifts["geese"], 
+       christmas_gifts["rings"],christmas_gifts["birds"], christmas_gifts["hens"],
+       christmas_gifts["turtle"],christmas_gifts["partridge"])
+
+print ("On the tenth day of Christmas my true love gave to me")
+print (christmas_gifts["lords"], christmas_gifts["ladies"], christmas_gifts["swans"], 
+       christmas_gifts["geese"], christmas_gifts["rings"],christmas_gifts["birds"], 
+       christmas_gifts["hens"],christmas_gifts["turtle"],christmas_gifts["partridge"])
+
+print ("On the eleventh day of Christmas my true love gave to me")
+print (christmas_gifts["pipers"], christmas_gifts["lords"], christmas_gifts["ladies"], 
+       christmas_gifts["swans"],christmas_gifts["geese"], christmas_gifts["rings"],
+       christmas_gifts["birds"],christmas_gifts["hens"],christmas_gifts["turtle"],
+       christmas_gifts["partridge"])
+
+print ("On the twelfth day of Christmas my true love gave to me")
+print (christmas_gifts["drummers"], christmas_gifts["pipers"], christmas_gifts["lords"], 
+       christmas_gifts["ladies"], christmas_gifts["swans"],christmas_gifts["geese"], 
+       christmas_gifts["rings"],christmas_gifts["birds"],christmas_gifts["hens"],
+       christmas_gifts["turtle"], christmas_gifts["partridge"])
