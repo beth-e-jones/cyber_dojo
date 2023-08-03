a10c4bdd7a861379d74ea65780f291b7c9de09ed
Beth Jones <beth.e.jones@ons.gov.uk>
2023-08-03 13:22:45 +0100

    fixed error where numbers didn't print

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,17 +1,18 @@
 #Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and
 # for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
-for fizzbuzz in range (1,100): #for some reason, the last printed thing is always the second number in this range minus one. fix!
+for fizzbuzz in range (1,100): 
     if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-            print ("fizzbuzz")
+        print("fizzbuzz")
         
     elif fizzbuzz % 3 == 0:
-        print ("fizz")
+        print("fizz")
     
     elif fizzbuzz % 5 == 0:
-        print ("buzz")
+        print("buzz")
     
-#need to find way of printing the other numbers as at the moment i've just told it to print if these conditions apply
-# i don't know how to get it to give me everything else too      
+    else:
+        print(fizzbuzz)
+    
+#Original error had the print outside the loop so it wasn't printing the number. For future, make sure else is in the loop
 
-print(fizzbuzz)
