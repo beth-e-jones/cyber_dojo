161d8b65123ff81ad393d2db6d6a81a72461ff9b
Beth Jones <beth.e.jones@ons.gov.uk>
2023-09-12 11:09:39 +0100

    added document

---
+++ fizzbuzz plus.py
@@ -0,0 +1,25 @@
+#Write a program that prints the numbers from 1 to 100, but...
+
+#numbers that are exact multiples of 3, or that contain 3, must print a string containing "Fizz"
+#For example 9 -> "...Fizz..."
+ #For example 31 -> "...Fizz..."
+
+#numbers that are exact multiples of 5, or that contain 5, must print a string containing "Buzz"
+ #For example 10 -> "...Buzz..."
+  #For example 52 -> "...Buzz..."
+
+for fizzbuzz in range (1,100): 
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+    
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+    
+    else:
+        print(fizzbuzz)
+    
+
+     
