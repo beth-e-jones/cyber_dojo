fe19f833fd15a6474f02ee8e04890b16bcad2cf4
Beth Jones <beth.e.jones@ons.gov.uk>
2023-09-25 15:29:23 +0100

    bug with negative numbers

--- closest_to_zero.py
+++ closest_to_zero.py
@@ -2,17 +2,25 @@
 #If there is a tie, choose the positive value.
 
 # %%
-""" Route one option - just finds min and prints it"""
-list_numbers = [11, -3, 5, 235, 8]
+""" Route one option - just finds min and prints it. 
+Need function solution later"""
+list_numbers = [11, -4, 3, 5, 235, 8]
 
 # %%
+# define smallest number as the minimum
 smallest_number = min(list_numbers)
 
 # %%
-print(f"The closest number to zero is {smallest_number}")
-
-
+""" This doesn't prioritise positive numbers over negative. Needs fix"""
 
 # %%
-""" This previous solution doesn't account for minus numbers. 
-I'll need a solution to identify minus numbers """
+for int in list_numbers:
+    if smallest_number < 0:
+        smallest_number = (smallest_number * -1)
+
+print(smallest_number)
+
+# %%
+# print result
+print(f"The closest number to zero is {smallest_number}")
+# %%
