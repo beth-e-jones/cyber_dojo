200dc12d8d8ae7f4e5f1966c515b8da21229f5b9
Beth Jones <beth.e.jones@ons.gov.uk>
2023-09-27 11:58:20 +0100

    fizzbuzz printed in one line

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,18 +1,24 @@
 #Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and
 # for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
-for fizzbuzz in range (1,100): 
-    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-        print("fizzbuzz")
+for fizzbuzz in range (1,100):
+    # Create empty string for each iteration to print fizzbuzz
+    empty_string = ""
+    if fizzbuzz % 3 == 0:
+        # Update empty string to add fizz if conditions met
+        empty_string = empty_string + "fizz"
         
-    elif fizzbuzz % 3 == 0:
-        print("fizz")
+    if fizzbuzz % 5 == 0:
+        # Update empty string to add buzz if conditions met
+        empty_string = empty_string + "buzz"
     
-    elif fizzbuzz % 5 == 0:
-        print("buzz")
-    
-    else:
+    # If something is in the string (e.g., fizz or buzz, print that)
+    if empty_string != "":
+        print(empty_string)
+        
+# If neither fizz or buzz conditions apply, print the number
+        
+    if fizzbuzz % 3 != 0 and fizzbuzz % 5 != 0:
         print(fizzbuzz)
     
 #Original error had the print outside the loop so it wasn't printing the number. For future, make sure else is in the loop
-
