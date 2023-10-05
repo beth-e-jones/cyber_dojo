f1af1f42fc5161ed5e620dff3f532d5e54651cd3
Beth Jones <beth.e.jones@ons.gov.uk>
2023-10-05 17:10:03 +0100

    drafting initial thoughts on solution

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -8,4 +8,18 @@ from list. Here are some examples:
 [1,1,2,2,3,3] -> [1,2,3]
 
 Author: Ramya V, KGISL
-"""
+""" 
+number_list = [2, 2, 3, 5, 7, 7, 8]
+def duplicate_removal:
+    for i in number_list:
+        empty_list=[]
+        
+        if i in
+
+
+duplicate_removal() 
+
+# import initial list
+# create blank list
+# move stuff into blank list
+# if it alreayd exists in list, move over it?
